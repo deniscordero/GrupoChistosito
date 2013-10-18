0ceee55cbe3f69ba51d30a9e72307f1bd72f42d2
denis.mejia <denis.mejia@W8CAPA10.creativa.com>
2013-10-18 08:11:50 -0600

    CONEXION BASE DE CREATIVA

--- ProductoMedicamento/src/com/creativa/producto/datasourse/DataSourceFactory.java
+++ ProductoMedicamento/src/com/creativa/producto/datasourse/DataSourceFactory.java
@@ -14,9 +14,9 @@ public class DataSourceFactory {
 		try {
 			oracleDataSource = new OracleDataSource();
 			oracleDataSource.setDriverType("thin");
-			oracleDataSource.setServerName("localhost");
+			oracleDataSource.setServerName("WS2008R264-ORA");
 			oracleDataSource.setPortNumber(1521);
-			oracleDataSource.setDatabaseName("xe");
+			oracleDataSource.setDatabaseName("ORA11DB");
 			oracleDataSource.setUser("curso01");
 			oracleDataSource.setPassword("curso01");
 			dataSource = oracleDataSource;
@@ -25,4 +25,21 @@ public class DataSourceFactory {
 			e.printStackTrace();
 		}
 	}
+	
+//	static{
+//		OracleDataSource oracleDataSource;
+//		try {
+//			oracleDataSource = new OracleDataSource();
+//			oracleDataSource.setDriverType("thin");
+//			oracleDataSource.setServerName("localhost");
+//			oracleDataSource.setPortNumber(1521);
+//			oracleDataSource.setDatabaseName("xe");
+//			oracleDataSource.setUser("curso01");
+//			oracleDataSource.setPassword("curso01");
+//			dataSource = oracleDataSource;
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
 }
